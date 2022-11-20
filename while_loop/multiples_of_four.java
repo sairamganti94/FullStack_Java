44b6de01d6d5353de275a9a170744a55e7f9749c
sairamganti94 <118657873+sairamganti94@users.noreply.github.com>
2022-11-20 15:34:41 +0530

    Create multiples_of_four.java

---
+++ while_loop/multiples_of_four.java
@@ -0,0 +1,22 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int i=4;
+        while(i <= N)
+        {
+            System.out.print(i+ " ");
+            i=i+4;
+            
+        }
+        
+    }
+}
